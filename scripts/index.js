0f65a3e9aae9e5cc00a89881d468f7386e11ee41
Sam Sharma <samsharma.dev@gmail.com>
2024-03-23 08:20:27 -0700

    update profile datat programmatically

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,50 @@
 const pic = document.querySelector(".profile-card__img");
-const username = document.querySelector(".profile-card__name");
+const usernames = document.querySelectorAll(".profile__name");
 const job = document.querySelector(".profile-card__job");
 
-pic.innerHTML = `<img src="./assets/images/woman.png" />`
-username.innerHTML = "Miranda Mountain";
+pic.innerHTML = `<img src="./assets/images/woman.png" />`;
+
+for(let el of usernames) {
+    el.innerHTML = "Miranda Mountain";
+}
+
 job.innerHTML = "Registered Psychologist";
+
+
+const  commentsArr = [];
+
+const displayComment = () => {
+    const post = document.querySelector(".posts__item");
+    commentsArr.forEach((comment) => {
+        const commentName = document.createElement('div');
+        commentName.classList.add("posts__username");
+        commentName.innerText = comment.name;
+        post.appendChild(commentName);
+
+        const commentContent = document.createElement("div");
+        commentContent.classList.add("posts__content");
+
+        const commentBody = document.createElement("p");
+        commentBody.innerText = comment.txt;
+        commentContent.appendChild(commentBody);
+
+        const likeButton = documet.createElement("div");
+        likeButton.classList.add("posts__like");
+        likeButton.innerHTML = `<button>Like</button>`;
+        post.appendChild(likeButton);
+    })
+}
+
+const postButton = document.querySelector(".post__button");
+
+postButton.addEventListener("click", (postObj) => {
+    postObj.preventDefault();
+
+    const newComment = {};
+    const commentTxt = document.querySelector(".comment__input");
+    newComment.txt = postObj.target.name.value;
+
+    commentsArr.unshift(newComment);
+    displayComment();
+} )
+
