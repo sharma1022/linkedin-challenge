9b2454d02aabb23ceb041acdf88c8ab9d972520e
Sam Sharma <samsharma.dev@gmail.com>
2024-03-23 07:36:18 -0700

    chnaged the name job title and pic  using dom

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const pic = document.querySelector(".profile-card__img");
+const username = document.querySelector(".profile-card__name");
+const job = document.querySelector(".profile-card__job");
+
+pic.innerHTML = `<img src="./assets/images/woman.png" />`
+username.innerHTML = "Miranda Mountain";
+job.innerHTML = "Registered Psychologist";
